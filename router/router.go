3ae93be9dd024ad421865ef48287d34ea0b56572
LuisMarchio03 <luisgabrielmarchio75@gmail.com>
2023-04-02 18:29:14 -0300

    feat: initialize routes

--- router/router.go
+++ router/router.go
@@ -5,11 +5,12 @@ import (
 )
 
 func Initialize() {
+	// Initialize Router
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+
+	// Initialize Routes
+	initializeRoutes(r)
+
+	// Run the server
 	r.Run(":8080")
 }
