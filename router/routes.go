8f9251944db9db691a64eb42c6b25e97b0798afe
LuisMarchio03 <luisgabrielmarchio75@gmail.com>
2023-04-03 14:23:58 -0300

    feat: add crud opening routes

--- router/routes.go
+++ router/routes.go
@@ -14,5 +14,40 @@ func initializeRoutes(r *gin.Engine) {
 				"msg": "GET Opening",
 			})
 		})
+		v1.POST("/opening", func(ctx *gin.Context) {
+			ctx.JSON(http.StatusOK, gin.H{
+				"msg": "POST Opening",
+			})
+		})
+		v1.DELETE("/opening", func(ctx *gin.Context) {
+			ctx.JSON(http.StatusOK, gin.H{
+				"msg": "DELETE Opening",
+			})
+		})
+		v1.PUT("/opening", func(ctx *gin.Context) {
+			ctx.JSON(http.StatusOK, gin.H{
+				"msg": "PUT Opening",
+			})
+		})
+		v1.GET("/openings", func(ctx *gin.Context) {
+			ctx.JSON(http.StatusOK, gin.H{
+				"msg": "GET Openings",
+			})
+		})
 	}
 }
+
+// GET
+//? curl -X GET http://localhost:8080/api/v1/opening
+
+// POST
+//? curl -X POST http://localhost:8080/api/v1/opening
+
+// DELETE
+//? curl -X DELETE http://localhost:8080/api/v1/opening
+
+// PUT
+//? curl -X PUT http://localhost:8080/api/v1/opening
+
+// GET
+//? curl -X GET http://localhost:8080/api/v1/openings
