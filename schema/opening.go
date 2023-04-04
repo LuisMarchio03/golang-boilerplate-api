6658626cbc988b9193152f57abff1087da319b64
LuisMarchio03 <luisgabrielmarchio75@gmail.com>
2023-04-04 11:57:46 -0300

    feat: add opening schema package

---
+++ schema/opening.go
@@ -0,0 +1,13 @@
+package schema
+
+import "gorm.io/gorm"
+
+type Opening struct {
+	gorm.Model
+	Role     string
+	Company  string
+	Location string
+	Remote   bool
+	Link     string
+	Salary   int64
+}
