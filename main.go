d1c994fe43716408503b7aeea828308d54508b6a
LuisMarchio03 <luisgabrielmarchio75@gmail.com>
2023-04-04 11:58:00 -0300

    feat: add custom logger

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "github.com/LuisMarchio03/golang-boilerplate-api/router"
+import (
+	"github.com/LuisMarchio03/golang-boilerplate-api/config"
+	"github.com/LuisMarchio03/golang-boilerplate-api/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	logger = config.GetLogger("main")
+
+	// Initialize the config
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("config initialization error: %v", err)
+		return
+	}
+
+	// Initialize the router
 	router.Initialize()
 }
